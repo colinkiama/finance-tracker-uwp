0102f12d0548b56f7bf8df9ff901acf9d2dac292
Colin Kiama <colinkiama@gmail.com>
2019-09-16 22:01:33 +0100

    Created IFileIOService interface

---
+++ src/FinanceTrackerUWP/FinanceTrackerLib/Services/IFileIOService.cs
@@ -0,0 +1,16 @@
+﻿using FinanceTrackerLib.Model;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTrackerLib.Services
+{
+    public interface IFileIOService
+    {
+        Task<List<FinanceAccount>> LoadFinananceAccountsAsync();
+        Task<List<Transaction>> LoadTransactionsAsync(FinanceAccount accountToLoadFrom);
+        Task SaveTransactionsAsync(IEnumerable<FinanceAccount> accountsToSave, IDictionary<string, List<Transaction>> accountTransactions);
+    }
+}
