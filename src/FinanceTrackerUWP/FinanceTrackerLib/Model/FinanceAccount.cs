ca24c2fe16a1b853f6a958735b34063fa5169e0e
Colin Kiama <colinkiama@gmail.com>
2019-09-16 21:26:03 +0100

    Created FinanceAccount class

---
+++ src/FinanceTrackerUWP/FinanceTrackerLib/Model/FinanceAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace FinanceTrackerLib.Model
+{
+    public class FinanceAccount : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private double _currentBalance;
+
+        public double CurrentBalance
+        {
+            get { return _currentBalance; }
+            set
+            {
+                _currentBalance = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Name { get; set; }
+
+
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
