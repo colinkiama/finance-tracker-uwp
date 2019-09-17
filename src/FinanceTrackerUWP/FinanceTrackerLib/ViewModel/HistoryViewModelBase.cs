9991b37e51157c87955b9a236d2f48f2206e8c4d
Colin Kiama <colinkiama@gmail.com>
2019-09-17 13:56:22 +0100

    Added ViewModel bases for new transactions and transaction history

---
+++ src/FinanceTrackerUWP/FinanceTrackerLib/ViewModel/HistoryViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using FinanceTrackerLib.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTrackerLib.ViewModel
+{
+    public abstract class HistoryViewModelBase : ViewModelBase
+    {
+        private IEnumerable<Transaction> _transactions;
+
+        public IEnumerable<Transaction> Transactions
+        {
+            get { return _transactions; }
+            set
+            {
+                _transactions = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+    }
+}
---
+++ src/FinanceTrackerUWP/FinanceTrackerLib/ViewModel/TransactionViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using FinanceTrackerLib.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTrackerLib.ViewModel
+{
+    public abstract class TransactionViewModelBase : ViewModelBase
+    {
+        private TransactionType _transactionType;
+
+        public TransactionViewModelBase(TransactionType transactionType)
+        {
+            _transactionType = transactionType;
+        }
+
+    }
+}
