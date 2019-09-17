80c30927563c125d0fb2be566ad9a8ef7a381fcd
Colin Kiama <colinkiama@gmail.com>
2019-09-17 12:57:01 +0100

    Created ViewModelBases to (potentially) share across multiple projects in the future

---
+++ src/FinanceTrackerUWP/FinanceTrackerLib/ViewModel/FinancesViewModelBase.cs
@@ -0,0 +1,36 @@
+﻿using FinanceTrackerLib.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace FinanceTrackerLib.ViewModel
+{
+    public abstract class FinancesViewModelBase : ViewModelBase
+    {
+        private FinanceAccount _currentAccount;
+
+        public FinanceAccount CurrentAccount
+        {
+            get { return _currentAccount; }
+            set
+            {
+                _currentAccount = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private IEnumerable<Transaction> _recentTransactions;
+
+        public IEnumerable<Transaction> RecentTransactions
+        {
+            get { return _recentTransactions; }
+            set
+            {
+                _recentTransactions = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+    }
+}
